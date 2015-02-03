e90a3892117a1a630f6028e1f5b1928cd393763a
Justin Phelps <linuturk@onitato.com>
2015-02-03 13:52:42 -0600

    adding artifact collection

--- test/fabric/ansible.py
+++ test/fabric/ansible.py
@@ -1,16 +1,17 @@
-import os
-from fabric.api import env, run, task, get
+from fabric.api import env, task
 from envassert import detect, file, group, package, port, process, service, \
     user
+from hot.utils.test import get_artifacts
 
 
 @task
 def check():
     env.platform_family = detect.detect()
-    release = run("/usr/bin/lsb_release -r | awk {'print $2'}")
 
-    assert package.installed("ansible-tower"), "package ansible-tower is not installed"
-    assert file.exists("/etc/tower/settings.py"), "/etc/tower/settings.py does not exist"
+    assert package.installed("ansible-tower"),\
+        "package ansible-tower is not installed"
+    assert file.exists("/etc/tower/settings.py"),\
+        "/etc/tower/settings.py does not exist"
     assert port.is_listening(80), "port 80 is not listening"
     assert port.is_listening(443), "port 443 is not listening"
     assert user.exists("awx"), "user awx does not exist"
@@ -21,27 +22,13 @@ def check():
     assert process.is_up("supervisor"), "process supervisor is not running"
     assert service.is_enabled("apache2"), "service apache2 is not enabled"
     assert service.is_enabled("postgres"), "service postgres is not enable"
-    assert service.is_enabled("redis-server"), "service redis-server is not enabled"
-    assert service.is_enabled("supervisor"), "service supervisor is not enabled"
+    assert service.is_enabled("redis-server"),\
+        "service redis-server is not enabled"
+    assert service.is_enabled("supervisor"),\
+        "service supervisor is not enabled"
 
 
 @task
 def artifacts():
     env.platform_family = detect.detect()
-
-    # Logs to pull
-    logs = ['/root/cfn-userdata.log',
-            '/root/heat-script.log']
-
-    # Artifacts target location
-    try:
-        os.environ['CIRCLE_ARTIFACTS']
-    except:
-        artifacts = 'tmp'
-    else:
-        artifacts = os.environ['CIRCLE_ARTIFACTS']
-
-    # For each log, get it down
-    for log in logs:
-        target = artifacts + "/%(host)s/%(path)s"
-        get(log, target)
+    get_artifacts()
